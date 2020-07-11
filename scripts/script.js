57f3b7699546b03301a897e9f48260ff41446178
sebastianbrosch <coding@sebastianbrosch.de>
2020-07-11 17:23:24 +0200

    no it is possible to switch between languages

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,25 @@
 $(document).ready(function() {
+    SetCurrentLanguage();
 
     //highlight the code on page load.
     $('pre code#code-formatted').each(function() {
-        hljs.highlightBlock($(this).get(0));
+        ReloadCodeContent();
     });
 
     //load the content of the textarea to the formatted area on every change.
     $('#code-input').on('input', function(e) {
-        $('#code-formatted').text($(this).val());
-        hljs.highlightBlock($('#code-formatted').get(0));
+        ReloadCodeContent();
     });
+
+    //get the language of the current selection after change.
+    $('select#select-language').change(function() {
+        SetCurrentLanguage();
+        ReloadCodeContent();
+    })
 
     //button click event to download the code.
     $("button#image-download").click(function() {
-        html2canvas($("#code-formatted").get(0)).then(canvas => {
+        html2canvas($("#code-formatted").get(0), {'logging': false}).then(canvas => {
             var itemHyperlink = document.createElement("a");
             itemHyperlink.href = canvas.toDataURL('image/png');
             itemHyperlink.setAttribute("download", 'code.png');
@@ -21,3 +27,11 @@ $(document).ready(function() {
         });
     });
 });
+
+function ReloadCodeContent() {
+    $('#code-formatted').text($('#code-input').val());
+    hljs.highlightBlock($('pre code#code-formatted').get(0));
+}
+function SetCurrentLanguage() {
+    $('pre code#code-formatted').attr('class', 'lang-' + ($('select#select-language').find('option:selected').val() || ''));
+}
